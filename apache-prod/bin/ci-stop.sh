4c93852ab01cd4b97f29901a010e564c8c826d26
Becky Reamy <rlreamy@med.umich.edu>
2018-11-02 13:25:54 -0400

    KPMP-304: Removing old apache-qa and apache-prod directories

--- apache-prod/bin/ci-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This file will run a container in the background
-source common.bash .
-
-echo "Cleaning up Docker image($maintainer/$imagename)"
-docker stop $imagename >> /dev/null
-docker rm $imagename
