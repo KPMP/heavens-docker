7056cc9e738e64db56e4c3c5ea00d746d4c2154c
albarkel@umich.edu <albarkel@umich.med.edu>
2021-10-26 18:11:44 -0400

    add counter, timers, and dual downs and ups

--- scripts/docker_container_check.py
+++ scripts/docker_container_check.py
@@ -5,6 +5,7 @@ import requests
 from dotenv import load_dotenv
 import endpoint_check
 import docker_container_reboot
+import time
 
 load_dotenv()
 
@@ -17,47 +18,52 @@ slack_url = 'https://hooks.slack.com/services/' + slack_passcode
 docker_client = docker.from_env()
 
 
-def checkDockerContainers():
+def checkDockerContainers(checkAttempts=0):
+    checkLimit = 3
     running_containers = set()
     expected_containers = set(expected_containers_list)
     message = ""
 
-    containers = docker_client.containers.list()
-
-    for container in containers:
-        running_containers.add(container.name)
-
-    missing_containers = expected_containers.difference(running_containers)
-
-    if len(missing_containers) > 0:
-        message = "ALERT: The following containers are missing on " + \
-            environment + ": " + \
-            ', '.join(missing_containers) + \
-            ' - Attempting to restart containers...'
-        response = requests.post(slack_url, headers={
-            'Content-type': 'application/json', }, data='{"text":"' + message + '"}')
-        docker_container_reboot.restartContainers()
-        checkDockerContainers()
-        return
+    if(checkAttempts >= checkLimit):
+        message = 'Check attempts exceeded check limit, no longer attempting to restart containers'
+        response = requests.post(slack_url, headers={'Content-type': 'application/json', }, data='{"text":"' + message + '"}')
+        return message
     else:
-        message = "All containers are running as expected: " + \
-            ', '.join(running_containers)
-        print(message)
+        checkAttempts += 1
+        containers = docker_client.containers.list()
+        for container in containers:
+            running_containers.add(container.name)
+        missing_containers = expected_containers.difference(running_containers)
 
-        healthy = endpoint_check.isHealthy()
-        if(not healthy):
-            message = "ALERT: The following service is not responding " + \
-                environment + ' - Attempting to restart containers...'
-            response = requests.post(slack_url, headers={
-                'Content-type': 'application/json', }, data='{"text":"' + message + '"}')
+        if len(missing_containers) > 0:
+            message = "ALERT: The following containers are missing on " + \
+                environment + ": " + \
+                ', '.join(missing_containers) + \
+                ' - Attempting to restart containers...'
+
+            response = requests.post(slack_url, headers={'Content-type': 'application/json', }, data='{"text":"' + message + '"}')
+            
             docker_container_reboot.restartContainers()
-            checkDockerContainers()
+            time.sleep(30)
+            checkDockerContainers(checkAttempts)
+            return
         else:
-            message = "Service is running as expected on: " + environment
-            print(message)
-            return message
+            message = "All containers are running as expected: " + \
+                ', '.join(running_containers)
+    
+            healthy = endpoint_check.isHealthy()
 
+            if(not healthy):
+                message = "ALERT: The following service is not responding " + \
+                    environment + ' - Attempting to restart containers...'
+                response = requests.post(slack_url, headers={
+                    'Content-type': 'application/json', }, data='{"text":"' + message + '"}')
+                docker_container_reboot.restartContainers()
+                time.sleep(30)
+                checkDockerContainers(checkAttempts)
+            else:
+                message = "Service is running as expected on: " + environment
+                return message
 
 if __name__ == "__main__":
     checkDockerContainers()
-    print("completed container check")
