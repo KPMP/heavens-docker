efd4935452ce39918f2201aef4d24b52915fc281
Alex Barkell <albarkel@umich.edu>
2022-01-26 16:06:18 -0500

    no extra var

--- ara/logAggregator.sh
+++ ara/logAggregator.sh
@@ -5,10 +5,9 @@ USAGE="usage: logAggregator.sh [up|down]"
 cmd=$1
 if [ $cmd == "up" ]; then
 	cmd="up"
-    detatched="-d"
-    docker-compose -f "docker-compose.yml" "$cmd" detatched
+    docker-compose -f "docker-compose.yml" "$cmd" "-d"
     sleep 300
-    docker-compose -f "alert-docker-compose.yml" "$cmd" detatched
+    docker-compose -f "alert-docker-compose.yml" "$cmd" "-d"
 else
     docker-compose -f "docker-compose.yml" "$cmd"
     docker-compose -f "alert-docker-compose.yml" "$cmd"
