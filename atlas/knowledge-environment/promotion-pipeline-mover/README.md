d348df4aeda35d79d04c46997beb5b58cf94e325
Dert1129 <nathancreger1111@gmail.com>
2023-06-08 11:29:19 -0400

    switch to docker run commands

--- atlas/knowledge-environment/promotion-pipeline-mover/README.md
+++ atlas/knowledge-environment/promotion-pipeline-mover/README.md
@@ -1,28 +1,35 @@
 # Promotion-pipline-mover
-s3Moves is a docker container that takes in a file and uploads the file to Amazon S3.
+This is a docker container that takes in a file and uploads the file to Amazon S3.
 
 ## Getting started
-To use this docker container, pull the image from dockerhub `docker pull kingstonduo/promotion-pipeline-mover:1.0`
+Provide file to be moved to s3 inside of the container_files directory
 
 Copy `.env.example` and rename it to `.env` 
 
-Create a container_files directory inside this directory
-
-Provide file to be moved to s3 inside of the container_files directory
-
 AWS_KEY is the key for the user that has access to the bucket you want to upload to
 
-AWS_SECRET_KEY is the key given to the user that is only share once the user is created
+AWS_SECRET_KEY is the key given to the user that is only shared once time when the user is created
 
-CONTAINER_FILE_LOCATION is the absolute path the file inside of the container
-
-BUCKET_NAME is the name of the desired bucket uploading files to
-
-OBJECT_NAME will be the name of the file when it enters S3
-
-
+Build the image with the provided Dockerfile
 
 ## Using the container
-Simply run `docker-compose up` and the container will spin up. When the file is uploaded the container will automatically shutdown and provide a successful exit code (code 200)
 
-If CONTAINER_FILE_LOCATION or BUCKET_NAME are invalid the container will shutdown and return an exit code (code 148)
+In the command line `docker run --env-file ./.env <image_name> -f <file_name> -d </home/name_of_file> -b <bucket_name>`
+
+If any one of the arguments are invaild the container will shutdown and return an exit code (code 148).
+
+If the container is successful, the container will upload the file to the S3 bucket and return a successful exit code (code 200)
+
+To view these codes type `docker ps -a`
+
+# Notes
+
+If you want the file to be in a "directory" in Amazon S3, replace `<file_name>` in the command above with the directory to the file on your local machine.
+
+The file name MUST have no spaces or the command will fail
+
+the `file_dir` will always be /home/name_of_file since the Dockerfile copies files in ./container_files to /home/ inside the container
+
+# Example
+
+`docker run --env-file ./.env myimage/name:tag -f  -d /home/hello_world.txt -b my-bucket-name`
--- atlas/knowledge-environment/promotion-pipeline-mover/move_to_s3.py
+++ atlas/knowledge-environment/promotion-pipeline-mover/move_to_s3.py
@@ -3,34 +3,9 @@ import os
 from botocore.client import Config
 from dotenv import load_dotenv
 import sys
-import getopt
-def input_version_number(argv):
-    arg_name = ""
-    arg_dir = ""
-    arg_obj = ""
-    arg_help = "{0} -f <file_name>".format(argv[0]) + "{1} -d <file_dir>".format(argv[1]) + "{2} -o <obj_name>".format(argv[2])
-    
-    try:
-        opts, args = getopt.getopt(argv[1:], "h:f:d:o:", ["help", "file_name=", "file_dir=", "obj_name="])
-    except:
-        print(arg_help)
-        sys.exit(2)
-    
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print(arg_help)  # print the help message
-            sys.exit(2)
-        elif opt in ("-v", "--version"):
-            arg_ver = arg
-    if arg_ver == "":
-      generate_index()
-      
-    else:
-      generate_index(None, arg_ver)
-      
-load_dotenv()
-
+import argparse
 def upload_to_s3(file_name, file_directory, bucket_name):
+  load_dotenv()
   s3 = boto3.client("s3",
                     aws_access_key_id=os.environ.get('AWS_KEY'),
                     aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
@@ -39,21 +14,39 @@ def upload_to_s3(file_name, file_directory, bucket_name):
                     aws_access_key_id=os.environ.get('AWS_KEY'),
                     aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
                     config=Config(signature_version='s3v4'))
-  
   bucket_list = []
   for bucket in s3_resources.buckets.all():
     bucket_list.append(bucket.name)
-  
-  if bucket_name in bucket_list and os.path.exists(): 
+  if bucket_name in bucket_list and os.path.exists(file_directory): 
     s3.upload_file(
-    Filename=os.environ.get("CONTAINER_FILE_LOCATION"),
-    Bucket=os.environ.get("BUCKET_NAME"),
-    Key=os.environ.get("OBJECT_NAME")
+    Filename=file_directory,
+    Bucket=bucket_name,
+    Key=file_name
   )
     sys.exit(200)
-  
   else:
     sys.exit(404)
   
 if __name__ == "__main__":
-  upload_to_s3()
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "-f",
+    "--file_name",
+    required=True,
+    help="Name of file to be moved",
+  )
+  parser.add_argument(
+    '-d',
+    '--file_dir',
+    required=True,
+    help="file directory inside container"
+  )
+  parser.add_argument(
+    '-b',
+    '--bucket_name',
+    required=True,
+    help="Name of S3 bucket uploading to"
+  )
+  args = parser.parse_args()
+  # print(args.file_name, args.file_dir, args.bucket_name)
+  upload_to_s3(args.file_name, args.file_dir, args.bucket_name)
