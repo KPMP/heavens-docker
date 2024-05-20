aee010f022704e36d42b96885f88b81cc51de061
Becky Reamy <rlreamy@umich.edu>
2024-05-20 13:12:43 -0400

    KPMP-5379: Fix docker compose script

--- dataLake.sh
+++ dataLake.sh
@@ -14,78 +14,78 @@ if [ "$environment" == "" ] || [ "$cmd" == "" ]; then
 	echo $USAGE
 elif [ "$environment" == "dev" ] && [ "$2" == "down" ]; then
 	cd eridanus
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
 	cd ../orion
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
   cd ../libra
-  /usr/local/bin/docker-compose -f docker-compose.local.yml $cmd
+  /usr/local/bin/docker compose -f docker-compose.local.yml $cmd
 elif [ "$environment" == "dev" ] && [ "$2" == "up" ]; then
 	cd orion
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $cmd
   cd ../libra
-  /usr/local/bin/docker-compose -f docker-compose.local.yml $cmd
+  /usr/local/bin/docker compose -f docker-compose.local.yml $cmd
 elif [ "$environment" == "dev" ] && [ "$2" == "restart" ]; then
 	cd orion
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $down
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $down
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $down
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $down
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $down
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $down
   cd ../libra
-  /usr/local/bin/docker-compose -f docker-compose.local.yml $down
+  /usr/local/bin/docker compose -f docker-compose.local.yml $down
 	sleep 5s
 
 	cd ../orion
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $up
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $up
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $up
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $up
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.dev.yml $up
+	/usr/local/bin/docker compose -f docker-compose.dev.yml $up
   cd ../libra
-  /usr/local/bin/docker-compose -f docker-compose.local.yml $up
+  /usr/local/bin/docker compose -f docker-compose.local.yml $up
 elif [ $environment == "prod" ] && [ "$2" == "down" ]; then
 	cd eridanus
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 	cd ../orion
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 	cd ../dataLakeProxyServer
-	/usr/local/bin/docker-compose $cmd
+	/usr/local/bin/docker compose $cmd
 	cd ../libra
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 elif [ $environment == "prod" ] && [ "$2" == "up" ]; then
 	cd dataLakeProxyServer
-	/usr/local/bin/docker-compose $cmd
+	/usr/local/bin/docker compose $cmd
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 	cd ../orion
-	docker-compose -f docker-compose.prod.yml $cmd
+	docker compose -f docker-compose.prod.yml $cmd
 	cd ../libra
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $cmd
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $cmd
 elif [ $environment == "prod" ] && [ "$2" == "restart" ]; then
 	cd orion
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $down
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $down
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $down
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $down
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $down
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $down
 	sleep 5s
 
 	cd ../orion
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $up
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $up
 	cd ../eridanus
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $up
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $up
 	cd ../stateManager
-	/usr/local/bin/docker-compose -f docker-compose.prod.yml $up
+	/usr/local/bin/docker compose -f docker-compose.prod.yml $up
 else
 	echo $USAGE
 fi
